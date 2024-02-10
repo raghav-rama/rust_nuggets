c908eafd828b0893b35369f75bbb41cacc867d36
raghav-rama <hackerer528@gmail.com>
2024-02-10 23:11:11 +0530

    add: blocking_get_request.rs

---
+++ src/bin/blocking_get_request.rs
@@ -0,0 +1,20 @@
+use error_chain::error_chain;
+use reqwest::blocking::get;
+use std::io::Read;
+
+error_chain! {
+    foreign_links {
+        HttpRequest(reqwest::Error);
+        Io(std::io::Error);
+    }
+}
+
+fn main() -> Result<()> {
+    let mut response = get("https://httpbin.org/get")?;
+    let mut body = String::new();
+    response.read_to_string(&mut body)?;
+    println!("Status: {}", response.status());
+    println!("Headers:\n{:#?}", response.headers());
+    println!("Body: {}", body);
+    Ok(())
+}
