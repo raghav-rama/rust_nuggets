a21250c7bae0796738345aa404be068b4471b26b
raghav-rama <hackerer528@gmail.com>
2024-02-19 21:58:55 +0530

    test: add async_get_request_test_error

--- src/bin/tests/async_get_request_test.rs
+++ src/bin/tests/async_get_request_test.rs
@@ -13,4 +13,11 @@ mod async_get_request_test {
         assert_eq!(response.status().as_u16(), 200);
         Ok(())
     }
+
+    #[tokio::test]
+    async fn test_async_get_request_with_error() -> Result<()> {
+        let response = reqwest::get("https://httpbin.org/status/404").await?;
+        assert_eq!(response.status().as_u16(), 404);
+        Ok(())
+    }
 }
