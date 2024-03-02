2df4cf88c6568079c0c0b6f6b42c27652c673826
raghav-rama <hackerer528@gmail.com>
2024-03-03 01:23:05 +0530

    feat: add extrat_link.rs

---
+++ src/bin/extract_links.rs
@@ -0,0 +1,30 @@
+use error_chain::error_chain;
+use select::{
+    document::{self, Document},
+    predicate::Name,
+};
+
+error_chain! {
+    foreign_links {
+        Io(std::io::Error);
+        HttpRequest(reqwest::Error);
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let res = reqwest::get("https://www.rust-lang.org")
+        .await?
+        .text()
+        .await?;
+    Document::from(res.as_str())
+        .find(Name("a"))
+        .filter_map(|n| n.attr("href"))
+        .for_each(|x| println!("{}", x));
+    // let document = Document::from(res.as_str());
+    // for node in document.find(Name("a")) {
+    //     let link = node.attr("href").unwrap_or("None");
+    //     println!("{}", link);
+    // }
+    Ok(())
+}
