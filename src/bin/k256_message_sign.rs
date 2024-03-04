b434de18ce52d7ebf7edaa2d395f6ace2c4a93ba
raghav-rama <hackerer528@gmail.com>
2024-03-04 22:25:14 +0530

    add: k256_meesage_sign

---
+++ src/bin/k256_message_sign.rs
@@ -0,0 +1,20 @@
+use k256::ecdsa::{
+    signature::{SignerMut, Verifier},
+    Signature, SigningKey, VerifyingKey,
+};
+use rand::rngs::OsRng; // Recommended secure random number generator
+
+fn main() {
+    // Key generation
+    let mut secret_key = SigningKey::random(&mut OsRng);
+    let public_key = VerifyingKey::from(&secret_key);
+
+    // Sample message
+    let message = b"This is a test message";
+
+    // Signing
+    let signature: Signature = secret_key.sign(message);
+
+    // Verification
+    assert!(public_key.verify(message, &signature).is_ok());
+}
