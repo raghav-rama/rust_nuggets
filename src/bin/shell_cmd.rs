33993f180f4adbb25df1edc20129ad7f74ec286f
raghav-rama <hackerer528@gmail.com>
2024-04-24 23:04:01 +0530

    Refactor: shell command in shell_cmd.rs to use curl with a Unix socket

--- src/bin/shell_cmd.rs
+++ src/bin/shell_cmd.rs
@@ -3,7 +3,7 @@ use std::process::Command;
 
 fn main() {
     // Specify the shell command to run
-    let command = "ls";
+    let command = "curl --unix-socket /var/run/docker.sock -X GET http:/v1.45/containers/json";
 
     // Create a Command instance
     let output = Command::new("sh")
@@ -11,10 +11,14 @@ fn main() {
         .arg(command)
         .output()
         .expect("Failed to execute command");
-
     // Print the command output
     io::stdout().write_all(&output.stdout).unwrap();
 
+    let output = String::from_utf8_lossy(&output.stdout);
+
+    // Print the command output
+    println!("{}", output);
+
     // Print the command error (if any)
-    io::stderr().write_all(&output.stderr).unwrap();
+    // io::stderr().write_all(&output.stderr).unwrap();
 }
