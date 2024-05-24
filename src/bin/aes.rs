28fb0221214ac5b9472bd73f8774fff860461a15
raghav-rama <hackerer528@gmail.com>
2024-05-24 07:11:10 +0530

    fix(aes): errors and change implementation

--- src/bin/aes.rs
+++ src/bin/aes.rs
@@ -1,29 +1,35 @@
-use aes_gcm::aead::{Aead, NewAead};
-use aes_gcm::{AeadInPlace, Aes256Gcm, Key, Nonce};
-use rand::{rngs::OsRng, RngCore};
+extern crate aes;
+extern crate block_modes;
+extern crate hex;
+extern crate hex_literal;
+
+use aes::Aes128;
+use block_modes::block_padding::Pkcs7;
+use block_modes::{BlockMode, Cbc};
+use hex_literal::hex;
+
+type Aes128Cbc = Cbc<Aes128, Pkcs7>;
 
 fn main() {
-    let mut key = [0u8; 32]; // 256-bit key
-    OsRng.fill_bytes(&mut key);
-    let mut nonce = [0u8; 12]; // 96-bit nonce
-    OsRng.fill_bytes(&mut nonce);
-    let key = Key::from_slice(&key);
-    let nonce = Nonce::from_slice(&nonce);
+    let key = hex!("000102030405060708090a0b0c0d0e0f");
+    let iv = hex!("101112131415161718191a1b1c1d1e1f");
 
-    let cipher = Aes256Gcm::new(key);
-    let mut plaintext = b"This is a top secret message!".to_vec();
-    let mut tag = [0u8; 16];
-    let ciphertext = cipher
-        .encrypt_in_place_detached(nonce, &[], &mut plaintext, &mut tag)
-        .unwrap();
+    let plaintext = b"hello world AES!";
 
-    let mut decryptedtext = ciphertext.to_vec();
-    let cipher = Aes256Gcm::new(key);
-    cipher
-        .decrypt_in_place_detached(nonce, &[], &mut decryptedtext, &tag)
-        .expect("Decryption failed!");
+    let mut buffer = [0u8; 32];
+    let pos = plaintext.len();
+    buffer[..pos].copy_from_slice(plaintext);
 
-    println!("Plaintext: {}", String::from_utf8_lossy(&plaintext));
-    println!("Ciphertext: {:?}", ciphertext);
-    println!("Decryptedtext: {}", String::from_utf8_lossy(&decryptedtext));
+    let cipher = Aes128Cbc::new_from_slices(&key, &iv).unwrap();
+    let ciphertext = cipher.encrypt(&mut buffer, pos).unwrap();
+
+    println!("ciphertext: {:?}", hex::encode(ciphertext));
+
+    let cipher = Aes128Cbc::new_from_slices(&key, &iv).unwrap();
+    let decrypted_ciphertext = cipher.decrypt(&mut buffer).unwrap();
+
+    println!(
+        "decrypted ciphertext: {:?}",
+        std::str::from_utf8(decrypted_ciphertext).unwrap()
+    );
 }
